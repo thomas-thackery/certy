221d4a9024989a9d1502bc5c3c056acd6a8eb0db
Thomas Thackery <tthackery@gmail.com>
2018-10-02 22:12:39 +0000

    clean up input to build usable command

--- certy.sh
+++ certy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Please paste in the combined list of CSR details provided by the customer."
+echo "Please paste in the combined list of CSR details provided by the customer. When "
 a=""
 
 while read line
@@ -8,6 +8,8 @@ do
  a+="$line "
 done < "${1:-/dev/stdin}"
 # echo "pan site_certs.get_csr:"${a//$'\n'/}
-a=${a// /,}
+a=${a//-/\\-}
+a=${a//#/}
+a=${a/'Common name: '/'common_name='\'}
+a=${a/'DNS names: '/'',dns_names=\'}
 echo "pan site_certs.get_csr:"$a
-
