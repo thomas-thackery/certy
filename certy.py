5305bdd6eb23fdc73a3dba7460c082774dcc109a
Thomas Thackery <tthackery@gmail.com>
2018-10-30 00:44:51 +0000

    update email and org functions. needs more massaging

--- certy.py
+++ certy.py
@@ -18,11 +18,13 @@ def create_command(customer_details):
     command = ''
     for detail in range(len(customer_details)):
         customer_details[detail] = customer_details[detail].replace('#','')
-        customer_details[detail] = customer_details[detail].replace('-','\-')
+        #customer_details[detail] = customer_details[detail].replace('-','\-')
         customer_details[detail] = customer_details[detail].replace(',','\,')
         customer_details[detail] = customer_details[detail].strip()
         customer_details[detail] = common_name(customer_details[detail])
         customer_details[detail] = dns_names(customer_details[detail])
+        customer_details[detail] = emails(customer_details[detail])
+        customer_details[detail] = org(customer_details[detail])
         command += customer_details[detail] + ','
     return command
 
@@ -37,15 +39,37 @@ def common_name(detail):
 def dns_names(detail):
     if "dns names" in detail.lower():
         detail = detail.lower().replace("dns names","dns_names")
+    if "dns name" in detail.lower():
+        detail = detail.lower().replace("dns name","dns_names")
         detail = detail.replace(":","=")
         detail = detail.replace(",",":")
         detail = detail.replace(" ","")
     return detail
 
+# Email addresses boing-rwtegd@dsafj.com me@po-do.us
 def emails(detail):
+    if "Email addresses [optional]".lower() in detail.lower():
+        detail = detail.lower().replace("email addresses [optional]","emails=")
+    if "Email addresses".lower() in detail.lower():
+        detail = detail.lower().replace("email addresses","emails=")
+    if "E-mail address: Optional".lower() in detail.lower():
+        detail = detail.lower().replace("e-mail address: optional","emails=")
+        #detail = detail.replace(" ","20%")
+        detail = detail + "'"
+        detail = detail.replace(" ","")
+        #detail = detail.replace(":","=")
+        # detail = detail.replace(",",":")
+        detail = detail.replace(" ",":")
+        detail = detail + "'"
     return detail
 
 def org(detail):
+    if "Organization:" in detail:
+        detail = detail.replace("Organization:","org='")
+        detail = detail.lstrip()
+        detail = detail.replace("-","\-")
+        detail = detail.replace(",","\,")
+        detail = detail + "'"
     return detail
 
 def org_unit(detail):
@@ -60,11 +84,18 @@ def locality(detail):
 def province(detail):
     return detail
 
+def technical_detail(detail):
+
+    return detail
+
 print("pan site_certs.get_csr:" + create_command(customer_input))
 
 # test input
+# Email addresses boing-rwtegd@dsafj.com me@po-do.us
+
 # Common name: some-sub.example.com
  # DNS names: some-site.example.com, some-other-sub.example.com 
+# Email addresses [optional] boink-daslfj@2345267.io.co
 # Organization: Some-Org, Inc.
 # Organizational unit: IT-Brain, 
 # Country: US
